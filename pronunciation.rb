1ed4797b5152ae88446be9dccaf0a495f5440e94
chen <3217855084@qq.com>
2022-08-27 16:01:30 +0800

    Create  pronunciation.rb
    # Get the written pronunciation in Merriam-Webster format
    
    # And get audio playback information

---
+++ pronunciation.rb
@@ -0,0 +1,67 @@
+require 'json'
+
+require 'net/http'
+
+
+
+#$word = "voluminous"
+
+$word = "virtual"
+
+
+
+# Get the written pronunciation in Merriam-Webster format
+
+# And get audio playback information
+
+
+
+url = URI.parse("https://www.dictionaryapi.com/api/v3/references/collegiate/json/#$word?key=f83982f5-a08d-47e9-86e3-c12560ad1123")
+
+req = Net::HTTP::Get.new url 
+
+res = Net::HTTP.start(url.host, url.port, 
+
+        :use_ssl => url.scheme == 'https') {|http| http.request req}
+
+json = res.body 
+
+
+
+# So hard to format the json
+
+# relate type convert(String, Hash, Integer, Array)
+
+hash = JSON.parse(json)
+
+prs = hash[0].fetch("hwi").fetch("prs")
+
+$mw = prs[0].fetch("mw")
+
+$base_filename = prs[0].fetch("sound").fetch("audio")
+
+$subdirectory = $base_filename[0]
+
+if $base_filename[0].is_a?(Numeric) or $base_filename[0] == '_'
+
+	$subdirectory = "number"
+
+elsif $base_filename[0] == 'g' and $base_filename[1] == ''
+
+	$subdirectory = "gg"
+
+elsif $base_filename[0] == 'b' and $base_filename[1] == 'i' and $base_filename[1] == 'x'
+
+	$subdirectory = "bix"
+
+else 
+
+	$subdirectory = $base_filename[0]
+
+end
+
+audio = "https://media.merriam-webster.com/audio/prons/en/us/mp3/#$subdirectory/#$base_filename.mp3"
+
+puts $mw
+
+puts audio

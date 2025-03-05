a064dc87dc8544ec91cbbcf9132e9478effd3e57
tmpfor <63970026+chen172@users.noreply.github.com>
2025-03-05 14:15:41 +0800

    fix bug

--- generate_audio_sentence.rb
+++ generate_audio_sentence.rb
@@ -1,12 +1,10 @@
 if ARGV.length < 2
-	puts "Usage: #{__FILE__} sentence.txt [words.txt] piper_dir"
+	puts "Usage: #{__FILE__} words.txt piper_dir"
 	exit
 end
 
-$filename = ARGV[0]
-#$words = ARGV[1]
-$words = ARGV[0].gsub(/def_/, '')
-#$piper_dir = ARGV[2]
+$words = ARGV[0]
+$def_words = "def_" + ARGV[0]
 $piper_dir = ARGV[1]
 
 i = 0
@@ -20,12 +18,16 @@ end
 
 # save audio
 i = 0
-File.open($filename, "r") do |file|
+File.open($def_words, "r") do |file|
     file.each_line {|line|
         line = line.gsub(/<\/?[^>]*>/, "").strip
-        $base_filename = "def_" + data[i].strip
-        puts $base_filename
-        save_audio = "echo \"" + line + "\" | " + $piper_dir + "piper.exe --model " + $piper_dir+"en_US-hfc_female-medium.onnx --output_file " + "\"" + "#$base_filename.wav" + "\""
+        if line == "Number Words" or line == "Medical Words"
+            i = i + 1
+            next
+        end
+        $audio_filename = "def_" + data[i].strip.gsub(/\//, '_')
+        puts $audio_filename
+        save_audio = "echo \"" + line + "\" | " + $piper_dir + "piper.exe --model " + $piper_dir+"en_US-hfc_female-medium.onnx --output_file " + "\"" + "#$audio_filename.wav" + "\""
         system(save_audio)
         i = i + 1
     }
